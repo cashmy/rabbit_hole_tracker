923a3fc44a9e5957a7111f3a59ffb8eab7be7b71
Cash Myers <cmyers880@gmail.com>
2022-11-11 23:13:53 -0600

    feat: Joy layout design

---
+++ src/components/ColorSchemeToggle.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { useColorScheme } from '@mui/joy/styles';
+import Tooltip from '@mui/joy/Tooltip';
+import IconButton from '@mui/joy/IconButton';
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+
+export default function ColorSchemeToggle() {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return <IconButton size="sm" variant="outlined" color="primary" />;
+  }
+  return (
+    <Tooltip title="Toggle theme">
+      <IconButton
+        id="toggle-mode"
+        size="sm"
+        variant="outlined"
+        color="primary"
+        onClick={() => {
+          if (mode === 'light') {
+            setMode('dark');
+          } else {
+            setMode('light');
+          }
+        }}
+      >
+        {mode === 'light' ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+      </IconButton>
+    </Tooltip>
+  );
+}
