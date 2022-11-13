3ce1a5c8440fb530adb0526c72988ac1bcd475f1
Cash Myers <cmyers880@gmail.com>
2022-11-13 14:43:24 -0600

    feat: Combine Mui & Joy in themed components

--- src/theme.jsx
+++ src/theme.jsx
@@ -1,6 +1,78 @@
-import { extendTheme } from '@mui/joy/styles';
+import { deepmerge } from '@mui/utils';
+import { experimental_extendTheme as extendMuiTheme } from '@mui/material/styles';
+import colors from '@mui/joy/colors';
+import {
+  extendTheme as extendJoyTheme,
+  CssVarsProvider,
+  useColorScheme,
+} from '@mui/joy/styles';
 
-const theme = extendTheme({
+const muiTheme = extendMuiTheme({
+    // This is required to point to `var(--joy-*)` because we are using `CssVarsProvider` from Joy UI.
+    cssVarPrefix: 'joy',
+    colorSchemes: {
+      light: {
+        palette: {
+          primary: {
+            main: colors.blue[500],
+          },
+          grey: colors.grey,
+          error: {
+            main: colors.red[500],
+          },
+          info: {
+            main: colors.purple[500],
+          },
+          success: {
+            main: colors.green[500],
+          },
+          warning: {
+            main: colors.yellow[200],
+          },
+          common: {
+            white: '#FFF',
+            black: '#09090D',
+          },
+          divider: colors.grey[200],
+          text: {
+            primary: colors.grey[800],
+            secondary: colors.grey[600],
+          },
+        },
+      },
+      dark: {
+        palette: {
+          primary: {
+            main: colors.blue[600],
+          },
+          grey: colors.grey,
+          error: {
+            main: colors.red[600],
+          },
+          info: {
+            main: colors.purple[600],
+          },
+          success: {
+            main: colors.green[600],
+          },
+          warning: {
+            main: colors.yellow[300],
+          },
+          common: {
+            white: '#FFF',
+            black: '#09090D',
+          },
+          divider: colors.grey[800],
+          text: {
+            primary: colors.grey[100],
+            secondary: colors.grey[300],
+          },
+        },
+      },
+    },
+  });
+
+const joyTheme = extendJoyTheme({
     colorSchemes: {
         light: {
             palette: {
@@ -20,5 +92,7 @@ const theme = extendTheme({
         }
     },
   });
+
+  const theme = deepmerge(muiTheme, joyTheme);
 
   export default theme;
